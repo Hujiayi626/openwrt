0cb632822a68393320f672d6a2cb3ac5f10d1e6e
Hujiayi626 <hujiayi626@gmail.com>
2022-11-28 13:40:34 +0800

    Update diy-part.sh

--- build/Lede_source/diy-part.sh
+++ build/Lede_source/diy-part.sh
@@ -89,6 +89,7 @@ EOF
 
 # 整理固件包时候,删除您不想要的固件或者文件,让它不需要上传到Actions空间（根据编译机型变化,自行调整需要删除的固件名称）
 cat >"$CLEAR_PATH" <<-EOF
+packages
 config.buildinfo
 feeds.buildinfo
 openwrt-x86-64-generic-kernel.bin
@@ -96,4 +97,8 @@ openwrt-x86-64-generic.manifest
 openwrt-x86-64-generic-squashfs-rootfs.img.gz
 sha256sums
 version.buildinfo
+ipk.tar.gz
+openwrt-x86-64-generic-ext4-combined.img.gz
+openwrt-x86-64-generic-ext4-rootfs.img.gz
+openwrt-x86-64-generic-rootfs.tar.gz
 EOF
